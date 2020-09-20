43f2439d59c66ffcd2a1d4edf36dbe5c8c3794a2
Alan Borges <sr.alan.bispo@gmail.com>
2020-09-20 20:27:06 -0300

    Client API + tests + doctests + docs

--- lib/elixir_queue/queue.ex
+++ lib/elixir_queue/queue.ex
@@ -1,19 +1,47 @@
 defmodule ElixirQueue.Queue do
   use GenServer
+  alias __MODULE__
 
-  def start_link(opts), do: GenServer.start_link(__MODULE__, opts)
+  def start_link(opts) do
+    name = Keyword.fetch!(opts, :name)
+    GenServer.start_link(__MODULE__, name, opts)
+  end
 
   @impl true
   def init(_opts) do
     {:ok, {}}
   end
 
+  @doc ~S"""
+  Get next job to be processed
+  ## Examples
+      iex> ElixirQueue.Queue.enqueue(%{foo: "bar"})
+      :ok
+  """
+  @spec enqueue(any) :: :ok
+  def enqueue(job), do: GenServer.call(Queue, {:enqueue, job})
+
+  @doc ~S"""
+  Get next job to be processed
+  ## Examples
+      iex> ElixirQueue.Queue.dequeue()
+      {:error, :nojob}
+      iex> ElixirQueue.Queue.enqueue(%{foo: "bar"})
+      :ok
+      iex> ElixirQueue.Queue.dequeue()
+      {:ok, %{foo: "bar"}}
+      iex> ElixirQueue.Queue.dequeue()
+      {:error, :nojob}
+  """
+  @spec dequeue :: {:ok, any} | {:error, :nojob}
+  def dequeue, do: GenServer.call(Queue, :dequeue)
+
   @impl true
   def handle_call({:enqueue, job}, _from, queue) do
     {:reply, :ok, Tuple.append(queue, job)}
   end
 
-  def handle_call(:dequeue, _from, {}), do: {:reply, {:ok, {}}, {}}
+  def handle_call(:dequeue, _from, {}), do: {:reply, {:error, :nojob}, {}}
 
   def handle_call(:dequeue, _from, queue) do
     job = elem(queue, 0)
--- test/elixir_queue_test.exs
+++ test/elixir_queue_test.exs
@@ -1,2 +1,18 @@
 defmodule ElixirQueueTest do
+  use ExUnit.Case
+  doctest ElixirQueue.Queue
+  alias ElixirQueue.Queue
+
+  setup do
+    Application.stop(:elixir_queue)
+    :ok = Application.start(:elixir_queue)
+  end
+
+  test "Queue.dequeue/0 with no job should get {:error, :nojob}" do
+    assert {:error, :nojob} = Queue.dequeue()
+  end
+
+  test "Queue.enqueue/1 should work" do
+    assert :ok = Queue.enqueue(%{foo: "test", baz: "test"})
+  end
 end
