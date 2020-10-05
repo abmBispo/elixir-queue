baa5698962af304548d8c191baddc787862610d7
Alan Borges <sr.alan.bispo@gmail.com>
2020-10-04 23:21:38 -0300

    docs + refactoring

--- lib/elixir_queue/worker_pool.ex
+++ lib/elixir_queue/worker_pool.ex
@@ -8,7 +8,6 @@ defmodule ElixirQueue.WorkerPool do
     Worker
   }
 
-  # Server side functions
   def start_link(opts) do
     name = Keyword.fetch!(opts, :name)
     GenServer.start_link(__MODULE__, name, opts)
@@ -27,67 +26,63 @@ defmodule ElixirQueue.WorkerPool do
     {:ok, %{pids: pids, successful_jobs: [], failed_jobs: []}}
   end
 
+  # Server side functions
   @impl true
-  def handle_call({:add_worker, pid}, _from, state = %{pids: workers}) do
-    {:reply, :ok, Map.put(state, :pids, [pid | workers])}
-  end
+  def handle_call({:add_worker, pid}, _from, state = %{pids: workers}),
+    do: {:reply, :ok, Map.put(state, :pids, [pid | workers])}
 
-  def handle_call(:workers, _from, state) do
-    {:reply, state.pids, state}
-  end
+  def handle_call(:workers, _from, state), do: {:reply, state.pids, state}
 
-  def handle_call(:failed_jobs, _from, state) do
-    {:reply, state.failed_jobs, state}
-  end
+  def handle_call(:failed_jobs, _from, state), do: {:reply, state.failed_jobs, state}
 
-  def handle_call(:successful_jobs, _from, state) do
-    {:reply, state.successful_jobs, state}
-  end
+  def handle_call(:successful_jobs, _from, state), do: {:reply, state.successful_jobs, state}
 
-  def handle_call({:add_successful_job, worker, job, result}, _from, state) do
-    {:reply, :ok,
-     Map.put(state, :successful_jobs, [{worker, job, result} | state.successful_jobs])}
-  end
+  def handle_call({:add_successful_job, worker, job, result}, _from, state),
+    do: {
+      :reply,
+      :ok,
+      Map.put(state, :successful_jobs, [{worker, job, result} | state.successful_jobs])
+    }
 
-  def handle_call({:add_failed_job, worker, job, err}, _from, state) do
-    {:reply, :ok, Map.put(state, :failed_jobs, [{worker, job, err} | state.failed_jobs])}
-  end
+  def handle_call({:add_failed_job, worker, job, err}, _from, state),
+    do: {:reply, :ok, Map.put(state, :failed_jobs, [{worker, job, err} | state.failed_jobs])}
 
   @impl true
-  def handle_info({:DOWN, _ref, :process, dead_worker, _reason}, %{
-        pids: pids,
-        successful_jobs: successful_jobs,
-        failed_jobs: failed_jobs
-      }) do
+  def handle_info({:DOWN, _ref, :process, dead_worker, _reason}, state) do
     {:ok, worker} = DynamicSupervisor.start_child(WorkerSupervisor, Worker)
     worker_reference = Process.monitor(worker)
-    pids = Enum.filter(pids, &(&1 != dead_worker))
+    pids = Enum.filter(state.pids, &(&1 != dead_worker))
 
     unless Mix.env() == :test,
-      do:
-        Logger.error("""
+      do: Logger.error("Unexpected worker error:
           Worker #{inspect(dead_worker)} received EXIT SIGNAL.
           It have been replaced by #{inspect(worker)} worker.
           All the job progress was lost and job failed.
-        """)
+        ")
 
-    {:noreply,
-     %{
-       pids: [{worker, worker_reference} | pids],
-       successful_jobs: successful_jobs,
-       failed_jobs: failed_jobs
-     }}
+    {:noreply, Map.put(state, :pids, [{worker, worker_reference} | pids])}
   end
 
-  def handle_info(_msg, state), do: {:noreply, state}
+  def handle_info(_msg, state),
+    do: {:noreply, state}
 
-  # Client side functions
+  # Client side functions #
+
+  @doc """
+  Adds the worker to `WorkerPool` state. Should receive a `PID`.
+  """
   @spec add_worker(pid()) :: :ok
   def add_worker(pid), do: GenServer.call(__MODULE__, {:add_worker, pid})
 
+  @doc """
+  Returns  _workers_ `PID`s kept in the state.
+  """
   @spec workers :: list()
   def workers, do: GenServer.call(__MODULE__, :workers)
 
+  @doc """
+  Returns  _workers_ `PID`s kept in the state.
+  """
   @spec failed_jobs :: list()
   def failed_jobs, do: GenServer.call(__MODULE__, :failed_jobs)
 
